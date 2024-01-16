97ad6c86bbd4f8a2d8a6b5f7013196b70f84638d
dmytrokashka <dmytro.kashka@beetroot.se>
2024-01-16 16:54:43 +0200

    tailwind config update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./templates/*.{liquid}",
-    "./layouts/*.{liquid}",
-    "./snippets/*.{liquid}",
+    "./templates/*.liquid",
+    "./layouts/*.liquid",
+    "./snippets/*.liquid",
   ],
-  theme: {
-    colors,
-    extend,
-  },
+  theme: {},
 }
