5a009345663df8ac37e625bf907d451836701e7e
dmytrokashka <dmytro.kashka@beetroot.se>
2024-02-07 11:54:55 +0200

    gulp setup

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require("gulp")
+const concat = require("gulp-concat")
+const uglify = require("gulp-uglify")
+const rename = require("gulp-rename")
+const watch = require("gulp-watch")
+
+const srcScripts = "src/scripts/*.js"
+const destDir = "assets"
+
+function bundleScripts() {
+  return gulp
+    .src(srcScripts)
+    .pipe(concat("bundle.js"))
+    .pipe(uglify())
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest(destDir))
+}
+
+function watchScripts() {
+  return watch(srcScripts, gulp.series(bundleScripts))
+}
+
+exports.build = bundleScripts
+exports.watch = gulp.series(bundleScripts, watchScripts)
